d6366cc1c666c4f7e4c7b6f49f19010a06fa41c1
LVSag <vansangle0405@gmail.com>
2026-05-29 07:26:50 +0700

    Revert "Merge master to main"

--- src/main/java/com/java/BaoCaoDoAn/Repository/DichVuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.BaoCaoDoAn.Repository;
-
-import com.java.BaoCaoDoAn.Model.DichVu;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DichVuRepository extends JpaRepository<DichVu, String> {
-}

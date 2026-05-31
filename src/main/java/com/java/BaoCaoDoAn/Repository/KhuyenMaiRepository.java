ab2e08a00a37777970cf253790861be711df7961
Vassy63 <oopstnt2005@gmail.com>
2026-05-31 23:44:37 +0700

    Cap nhat khuyen mai va hoa don - can tiep tuc fix logic ngay ap dung

--- src/main/java/com/java/BaoCaoDoAn/Repository/KhuyenMaiRepository.java
+++ src/main/java/com/java/BaoCaoDoAn/Repository/KhuyenMaiRepository.java
@@ -4,6 +4,10 @@ import com.java.BaoCaoDoAn.Model.KhuyenMai;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface KhuyenMaiRepository extends JpaRepository<KhuyenMai, Integer> {
+    Optional<KhuyenMai> findByMaCode(String maCode);
+    boolean existsByMaCode(String maCode);
 }
